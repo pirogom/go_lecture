09d13411d1d61abd086475cd6e3955acbe0b8b77
pirogom <mop.pirogom@gmail.com>
2024-02-04 20:21:35 +0900

    예제 추가

---
+++ 09_07_SliceMake/go.mod
@@ -0,0 +1,3 @@
+module github.com/pirogom/go_lecture/09_07_SliceMake
+
+go 1.21.5
---
+++ 09_07_SliceMake/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+}
+
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+}
