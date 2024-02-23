a99246577cc45af223694e7712eb6de03ccdff46
pirogom <mop.pirogom@gmail.com>
2024-02-24 01:42:17 +0900

    예제수정

--- 13_02_FunctionReturn/main.go
+++ 13_02_FunctionReturn/main.go
@@ -8,7 +8,17 @@ func functionReturn() func() string {
 	}
 }
 
+func someText() string {
+	return "피로곰의 Go언어 속성강의"
+}
+
+func functionReturn2() func() string {
+	return someText
+}
+
 func main() {
 	rf := functionReturn()
+	fmt.Println(rf())
+	rf = functionReturn2()
 	fmt.Println(rf())
 }
